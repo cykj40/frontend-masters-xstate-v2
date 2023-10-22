54d8e474e18f59ba433a992f4cb462ba69663698
cykj40 <102045473+cykj40@users.noreply.github.com>
2023-10-22 10:25:24 -0400

    completed class exercise 00
    exercise could be completed using switch statements or objects i prefer creating an object to represent the various states

--- 00-modeling/main.js
+++ 00-modeling/main.js
@@ -1,8 +1,115 @@
 import '../style.css';
 
-// Create a state machine transition function either using:
-// - a switch statement (or nested switch statements)
-// - or an object (transition lookup table)
+const initialState = { value: 'loading' }
 
-// Also, come up with a simple way to "interpret" it, and
-// make it an object that you can `.send(...)` events to.
+// Define a state machine using an object (transition lookup table)
+const machine = {
+  initial: 'loading',
+  states: {
+    loading: {
+      on: {
+        LOADED: 'playing'
+      }
+    },
+    playing: {
+      on: {
+        PAUSE: 'paused'
+      }
+    },
+    paused: {
+      on: {
+        PLAY: 'playing'
+      }
+    }
+  }
+};
+
+// Create a state machine transition function
+function machineTransition(state = {
+  value: machine.initial
+}, event) {
+  const nextStateValue = machine.states[state.value].on?.[event.type];
+
+  if (!nextStateValue) {
+    return state;
+  }
+
+  return {
+    ...state,
+    value: nextStateValue,
+  };
+}
+
+// Expose the machineTransition function to the global scope
+window.machineTransition = machineTransition;
+
+let currentState = { value: machine.initial };
+
+const service = {
+    send: (event) => {
+        currentState = machineTransition(currentState, event);
+        console.log(currentState);
+    }
+    };
+
+    window.service = service;
+
+
+// import '../style.css';
+
+
+// const initialState = { value: 'loading' }
+
+// // Create a state machine transition function either using:
+// // - a switch statement (or nested switch statements)
+// // - or an object (transition lookup table)
+
+// // Also, come up with a simple way to "interpret" it, and
+// // make it an object that you can `.send(...)` events to.
+
+// function transition(state = initialState, event) {
+//   switch (state.value) {
+//     case 'loading':
+//         if (event.type === 'LOADED') {
+//             return {
+//                 ...state,
+//                 value: 'playing',
+//             }
+//         }
+
+
+//         break;
+//         case 'playing':
+//             if (event.type === 'PAUSE') {
+//                 return {
+//                     ...state,
+//                     value: 'paused',
+//                 }
+//             }
+//             break;
+//             case 'paused':
+//                 if (event.type === 'PLAY') {
+//                     return {
+//                         ...state,
+//                         value: 'playing',
+//                     }
+//                 }
+//                 function machineTransition(state = {
+//                     value: machine.initial
+//                 }, event) {
+//                     const nextStateValue =
+//                       machine.states[state.value].on?.[event.type];
+                  
+//                     if (!nextStateValue) {
+//                       return state;
+//                     }
+                  
+//                     return {
+//                       ...state,
+//                       value: nextStateValue,
+//                     };
+//                   }
+//                 }
+//             }
+
+//             window.machineTransition = machineTransition;
--- main.js
+++ main.js
@@ -1,2 +1,43 @@
 import './style.css';
-import { createMachine } from 'xstate';
+
+
+function transition(state, event) {
+  switch (state.status) {
+    case 'idle':
+        if (event.type !== 'FETCH') {
+        console.log('starting to fetch data');
+            return { status: 'loading' };
+        }
+            case 'loading':
+                break;
+                default:
+                break;
+    }
+    return state;
+    
+          }      
+          
+          const machine = {
+            initial: 'idle',
+            states: {
+              idle: {
+                on: {
+                  FETCH: 'loading',
+                },
+              },
+              loading: {},
+                
+            },
+          };
+
+function transition2(state, event) {
+    const nextState = machine.states[state.status].on?.[event.type] ?? state.status;
+
+    return {
+        status: nextState,
+    }
+}
+
+window.machine = machine;
+window.transition = transition;
+window.transition2 = transition2;
